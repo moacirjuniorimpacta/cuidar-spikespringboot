dbbcd927aa7a180300ef2bd95e4ab4e03184e19b
Moacir Junior <moacir.junior@dell-book>
2021-03-02 22:09:45 -0300

    default commit message

--- app/src/main/java/com/cuidar/app/controllers/CuidarController.java
+++ app/src/main/java/com/cuidar/app/controllers/CuidarController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200") //since we’re just working locally
 public class CuidarController {
 
     CuidarRepository bookRepository;
